d4cff82fb0acec46e056405d8407b014dbac5ebb
groovypark <soobin950@gmail.com>
2019-08-30 15:27:58 +0900

    세팅

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # country-information
+
+## 참고 링크
+- [Webpack4 for React (리액트를 위한 웹팩4) - 1](https://velog.io/@padakim/Webpack4-for-React-%EB%A6%AC%EC%95%A1%ED%8A%B8%EB%A5%BC-%EC%9C%84%ED%95%9C-%EC%9B%B9%ED%8C%A94-1-)
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 8080;
+
+module.exports = {
+    // webpack 설정 코드 작성. 작성된 코드는 module.export로 내보냅니다.
+};
